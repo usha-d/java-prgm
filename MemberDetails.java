84855763aa7d9274cdde5923443d78545f0213b2
usha-d <72968310+usha-d@users.noreply.github.com>
2020-10-20 16:00:09 +0530

    Add files via upload

---
+++ MemberDetails.java
@@ -0,0 +1,43 @@
+class MemberDetails{
+	public static void main(String[] args){
+		Details varshitha= new Details();
+		varshitha.name="varshitha";
+		varshitha.place="Tarikere";
+		varshitha.college="AIT";
+		varshitha.hobbies="Drawing";
+		varshitha.DOB="09/04/1998";
+		varshitha.ListingDetails();
+
+		Details usha= new Details();
+		usha.name="Usha";
+		usha.place="Davanagere";
+		usha.college="AIT";
+		usha.hobbies="photography";
+		usha.DOB="31/12/1998";
+		usha.ListingDetails();
+
+		Details tasmiya= new Details();
+		tasmiya.name= "Tasmiya";
+		tasmiya.place= "Chikkamagaluru";
+		tasmiya.college= "AIT";
+		tasmiya.hobbies= "Reading";
+		tasmiya.DOB= "10/01/1998";
+		tasmiya.ListingDetails();
+
+		Details ramya= new Details();
+		ramya.name="Ramya";
+		ramya.place="Chikkamagaluru";
+		ramya.college="AIT";
+		ramya.hobbies="Drawing";
+		ramya.DOB="24/08/1998";
+		ramya.ListingDetails();
+
+		Details shrinivas= new Details();
+		shrinivas.name="Shrinivas";
+		shrinivas.place="Jamkhandi";
+		shrinivas.college="BGMIT";
+		shrinivas.hobbies="Cricket";
+		shrinivas.DOB="21/02/1998";
+		shrinivas.ListingDetails();
+}
+}
