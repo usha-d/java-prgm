5b425b4be22845998f822a62756dba32e2aa478c
usha-d <72968310+usha-d@users.noreply.github.com>
2020-10-22 20:23:36 +0530

    Add files via upload

---
+++ AirthmaticOperatorsDemo.java
@@ -0,0 +1,44 @@
+class AirthmaticOperatorsDemo{
+
+public static void main(String args[]){
+short n1=10;
+short n2=20;
+int n3=n1+n2;
+System.out.println("n1+n2="+n3);
+
+ int i1=20;
+int i2=10;
+int i3=i1-i2;
+System.out.println("i1-i2="+i3);
+
+
+
+ int k1=20;
+int k2=10;
+int k3=k1*k2;
+System.out.println("k1*k2="+k3);
+
+
+
+
+ int l1=20;
+int l2=10;
+int l3=l1/l2;
+System.out.println("l1/l2="+l3);
+
+
+ int x1=20;
+int x2=10;
+int x3=x1%x2;
+System.out.println("x1%x2="+x3);
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ IncrementOperatorDemo.java
@@ -0,0 +1,13 @@
+class IncrementOperatorDemo{
+public static void main(String args[]){
+int p=5;
+int q=4;
+int r=q++;
+System.out.println(r++);
+System.out.println(++q);
+System.out.println(p++);
+System.out.println(r);
+System.out.println(q);
+System.out.println(p);
+}
+}
